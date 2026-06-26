9e38a4b1b1d269ed29a709f1ae4fa6e82d61efa1
vrma <vrmachado@gmail.com>
2026-06-26 12:47:58 +0200

    estado inicial

---
+++ src/main/java/com/example/RestApiDemoMostolesBackend2026Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDemoMostolesBackend2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDemoMostolesBackend2026Application.class, args);
+	}
+
+}
