fdcc412c3410847a0cc51e2258297e9b2b2e24e2
vrma <vrmachado@gmail.com>
2026-06-26 14:26:23 +0200

    entidades creadas y correctamente relacionadas

---
+++ src/main/java/com/example/entities/Presentation.java
@@ -0,0 +1,43 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "presentations")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Presentation implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, mappedBy = "presentation")
+    private List<Product> products;
+}
---
+++ src/main/java/com/example/entities/Product.java
@@ -0,0 +1,61 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "El producto tiene que tener un nombre")
+    @NotEmpty(message = "El nombre del producto no puede estar vacio")
+    @Size(min = 4, max = 25, message = "El nombre del producto no puede tener menos de 4 caracteres ni mas de 25")
+    private String name;
+
+    @NotNull(message = "La description del producto es requerida")
+    @NotBlank(message = "La description del producto no puede tener espacios vacios solamente")
+    @Size(max = 45, message = "La description no puede superar los 45 caracteres")
+    private String description;
+
+    @Min(value = 0, message = "El stock del producto no puede ser negativo")
+    private int stock;
+
+    @Min(value = 0, message = "El precio no puede estar en valores negativos")
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Presentation presentation;
+}
