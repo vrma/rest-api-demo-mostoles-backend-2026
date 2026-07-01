5d8dbabfa2e678a425ac670b064f89b82d563b8e
vrma <vrmachado@gmail.com>
2026-07-01 12:24:19 +0200

    metodo que recupera producto por el id ha sido implementado

--- src/main/java/com/example/controllers/ProductController.java
+++ src/main/java/com/example/controllers/ProductController.java
@@ -1,9 +1,22 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Product;
@@ -42,12 +55,104 @@ public class ProductController {
 
     private final ProductService productService;
 
-    // Metodo que recibe una peticion para devolver un listado de todos los productos
+     /**
+     * 
+     * IMPORTANTE!!!
+     * 
+     * Una API REST tiene que devolver informacion respecto a como ha sido
+     * solucionada la peticion (request),
+     * por ejemplo: el codigo 200 significa estado OK de la peticion, el codido 201
+     * significaria CREATED,
+     * el codigo 500 significaria que el servidor no ha podido cumplimentar la
+     * peticion, el codigo 401 NO ENCONTRADO,
+     * el codigo 403 prohibido, etc. Todos estos codigos se pueden encontrar en el
+     * sitio de W3Schools
+     * 
+     * https://www.w3schools.com/tags/ref_httpmessages.asp
+     * 
+     */
+
+    /**
+     * El metodo siguiente va a responder a una peticion (request) del tipo:
+     * 
+     * http://localhost:8080/products?page=0&size=3
+     * 
+     * Donde los parametros page y size seran utilizados para la paginacion, y no
+     * seran requeridos, es decir,
+     * que no son obligatorios que se suministren. Y en caso de NO ser suministrados
+     * (page y size),
+     * los productos se van a devolver ordenados.
+     * 
+     */
     @GetMapping
-    public List<Product> getProducts() {
+    public ResponseEntity<Map<String, Object>> dameProductos(
+        @RequestParam(name = "page", required = false) Integer page,
+        @RequestParam(name = "size", required = false) Integer size ) {
 
-        List<Product> allProducts = productService.findAll();
+        List<Product> products = null;
+        Map<String, Object> responseAsMap = new HashMap<>();
+        Sort sort = Sort.by("name");
 
-        return allProducts;
+        // Comprobar si en la peticion (request) me han suministrado los parametros page y size
+        if (page != null && size != null) {
+
+            Pageable pageable = PageRequest.of(page, size, sort);
+
+            // Implica devolver los productos paginados, es decir, una pagina de Product
+            Page<Product> productPage = productService.findAll(pageable);
+            products = productPage.getContent();
+            responseAsMap.put("productos", products);
+
+        } else {
+
+            // Devolver los productos ordenados, por nombre (name), por ejemplo
+            products = productService.findAll(sort);
+            responseAsMap.put("productos", products);
+        }
+        
+        return new ResponseEntity<>(responseAsMap, HttpStatus.OK);
+    }
+
+    /**
+     * El metodo siguiente recupera un Producto por el id que se recibe como una variable en la ruta, 
+     * mediante un end point (url o uri) que tiene el formato siguiente:
+     * 
+     * http://localhost:8080/productos/1 
+     * 
+     * Donde el valor 1 al final del end point seria el id del producto
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> findProductById(
+        @PathVariable(name = "id", required = true) int product_id) {
+
+        Map<String, Object> responseAsMap = new HashMap<>();
+        ResponseEntity<Map<String, Object>> responseEntity = null;
+        
+        try {
+            Product product = productService.findById(product_id);
+            if (product != null) {
+                String successMessage = "El producto con id " + product_id +
+                                        " ha sido encontrado";
+                responseAsMap.put("mensaje todo OK: ", successMessage);
+                responseAsMap.put("producto encontrado: ", product);
+                responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, 
+                                         HttpStatus.OK);
+            } else {
+                String failureMessage = "No ha sido encontrado ningun producto con id: "
+                         + product_id;
+                responseAsMap.put("Error: ", failureMessage);
+                responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.NOT_FOUND);
+            }
+        } catch (DataAccessException e) {
+            String errorMessage = "Error grave al buscar el producto con id " 
+                    + product_id + ", y la causa mas probable es: " + 
+                    e.getMostSpecificCause().getMessage();
+            responseAsMap.put("Error grave: ", errorMessage);
+            responseEntity = new ResponseEntity<>(responseAsMap,
+                 HttpStatus.INTERNAL_SERVER_ERROR);
+        }    
+           
+
+        return responseEntity;
     }
 }
