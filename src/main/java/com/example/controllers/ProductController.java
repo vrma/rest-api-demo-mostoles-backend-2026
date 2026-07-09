b20ff8606e9f1e31348654f3aa805254a2483f17
vrma <vrmachado@gmail.com>
2026-07-09 12:06:03 +0200

    Creacion del criterio de ordenamiento Sort.by ... resuelto

--- src/main/java/com/example/controllers/ProductController.java
+++ src/main/java/com/example/controllers/ProductController.java
@@ -108,7 +108,8 @@ public class ProductController {
 
         List<Product> products = null;
         Map<String, Object> responseAsMap = new HashMap<>();
-        Sort sort = Sort.by("name");
+        String nombre = "name";
+        Sort sort = Sort.by(nombre);
 
         // Comprobar si en la peticion (request) me han suministrado los parametros page
         // y size
